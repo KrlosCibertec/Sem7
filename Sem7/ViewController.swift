174ec789ff8da71f9d6318e74923e769caaf1103
krlosCibertec <i201822031@cibertec.edu.pe>
2023-10-22 13:09:49 -0500

    Initial Commit

---
+++ Sem7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sem7
+//
+//  Created by krlos90 on 22/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
